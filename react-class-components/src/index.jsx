a8ea9cff9d2a8a5f2ace7fc42321fddef9e8a945
b-chavez <brandedhard@gmail.com>
2021-03-16 19:07:01 -0700

    removed constructor

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class CustomButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.text = props.text;
-  }
 
   render() {
     return (
