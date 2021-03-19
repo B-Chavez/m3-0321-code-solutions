a3192c97d9fb57367fed8808bff1f53cb8dc468d
b-chavez <brandedhard@gmail.com>
2021-03-18 21:31:42 -0700

    moved appropriate dependencies to devDependencies

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function hotButton() {
+    let buttonClass = 'hot';
+
     if(this.state.isClicked >= 18){
-        return <button className='hot6' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot6';
     }
     if(this.state.isClicked >= 15){
-        return <button className='hot5' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot5';
     }
     if(this.state.isClicked >= 12){
-        return <button className='hot4' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot4';
     }
     if(this.state.isClicked >= 9){
-        return <button className='hot3' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot3';
     }
     if(this.state.isClicked >= 6){
-        return <button className='hot2' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot2';
     }
     if(this.state.isClicked >= 3){
-        return <button className='hot1' onClick={this.handleClick}>Hot Button</button>
+        buttonClass = 'hot1'
         
     }
-    return <button className='hot' onClick={this.handleClick}>Hot Button</button>
+    return <button className={buttonClass} onClick={this.handleClick}>Hot Button</button>
 
 }
 
