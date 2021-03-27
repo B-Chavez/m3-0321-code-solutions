877b999f15ecefd706dbd764e1c575eee695fb7f
Brandon Chavez <brandedhard@gmail.com>
2021-03-27 16:14:55 -0700

    React Validated Input Component Completed

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class ValidatedInput extends React.Component{
+  constructor(prop){
+    super(prop);
+    this.state = { password: '', message: '', icon:'noIcon'}
+
+    this.validateText = this.validateText.bind(this);
+  }
+
+  validateText(event){
+    console.log(event.target.value);
+    const password = event.target.value;
+    if(!password){
+      console.log('A password is required.')
+      this.setState({message: 'A password is required.'});
+    } else if(password.length < 8 && password){
+      console.log('Password is not long enough')
+      this.setState({ message: 'Password is too short.', icon: 'xIcon'})
+    } else {
+      console.log('Password is acceptable.')
+      this.setState({message: '', icon: 'checkIcon'})
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="container">
+          <form>
+            <label>
+              <input type="text" onChange={this.validateText}/>
+            </label>
+          </form>
+          <div className={this.state.icon}></div>
+        </div>
+
+        <div id="message">{this.state.message}</div>
+      </div>
+
+    )
+  }
+}
+
+export default ValidatedInput;
